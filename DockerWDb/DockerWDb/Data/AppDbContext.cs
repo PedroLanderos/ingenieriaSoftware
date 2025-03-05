e07b9b3d29765a2a707a2d2d8c7b46c7eb4e1188
PedroLanderos <developerpjlc@gmail.com>
2025-03-05 09:17:28 -0600

    redefinition of IUser

--- DockerWDb/DockerWDb/Data/AppDbContext.cs
+++ DockerWDb/DockerWDb/Data/AppDbContext.cs
@@ -8,28 +8,5 @@ namespace DockerWDb.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<UserModel> Usuarios { get; set; }
-        public DbSet<Rol> Roles { get; set; }
-        public DbSet<UsuarioRol> UsuarioRoles { get; set; }
-
-        // Configuración de claves primarias y relaciones
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Definir clave primaria compuesta para la entidad UsuarioRol
-            modelBuilder.Entity<UsuarioRol>()
-                .HasKey(ur => new { ur.UsuarioId, ur.RolId });
-
-            // Configurar la relación muchos a muchos entre Usuario y Rol
-            modelBuilder.Entity<UsuarioRol>()
-                .HasOne(ur => ur.Usuario)
-                .WithMany(u => u.UsuarioRoles)
-                .HasForeignKey(ur => ur.UsuarioId);
-
-            modelBuilder.Entity<UsuarioRol>()
-                .HasOne(ur => ur.Rol)
-                .WithMany(r => r.UsuarioRoles)
-                .HasForeignKey(ur => ur.RolId);
-        }
     }
 }
--- DockerWDb/DockerWDb/Interfaces/IUser.cs
+++ DockerWDb/DockerWDb/Interfaces/IUser.cs
@@ -5,7 +5,7 @@ namespace DockerWDb.Interfaces
     public interface IUser
     {
         Task<UserModel> RegisterUser(UserModel user);
-        Task<UserModel> LoginUser(string email, string password);
+        Task<UserModel> LoginUser(LoginModel loginModel);
         Task<IEnumerable<UserModel>> GetAllUsers();
         Task<UserModel> GetUserById(long id);
         Task<bool> EditUser(UserModel user);
